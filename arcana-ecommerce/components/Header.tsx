c74ebdb992b67795d973b418af9213e8ba6741b0
Divine <divinelavente@gmail.com>
2025-02-09 15:23:35 +0800

    more setup

--- arcana-ecommerce/components/Header.tsx
+++ arcana-ecommerce/components/Header.tsx
@@ -3,10 +3,11 @@ import { FiSearch, FiShoppingCart, FiUser } from "react-icons/fi";
 
 export const Header = () => {
     return(
-        <div className="flex flex-row justify-between items-center p-4">
+        <div className="flex flex-row justify-between items-center p-4 bg-cream">
             <div className="flex flex-row gap-4">
                 <a>Products</a>
                 <a>About Us</a>
+
             </div>
             <div className="flex flex-row gap-4">
                 <FiSearch />
--- arcana-ecommerce/tailwind.config.js
+++ arcana-ecommerce/tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}"
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
       extend: {
         colors: {
           'one': "#FEFFFE",
           'two': "#F7F0E6",
+          'three': "#F3C1D4",
         }
       },
     },
+
     plugins: [],
-  };
+};
   
