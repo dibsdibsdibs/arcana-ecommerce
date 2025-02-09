2fd17d9f639999cf5feb7e8f6149e9e7f3b79d95
Divine <divinelavente@gmail.com>
2025-02-09 14:44:47 +0800

    more setup

--- arcana-ecommerce/pages/index.tsx
+++ arcana-ecommerce/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Header } from '../components/Header'
+import React from "react";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>
-      <Header />
+    <div className="bg-two">
+      <a>TEXT</a>
     </div>
   );
-};
 
-export default Home;
+
+}
