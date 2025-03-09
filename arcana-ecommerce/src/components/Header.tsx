bd8dc81c8907e8e6ccac839389ef153c3fc139ce
Divine <divinelavente@gmail.com>
2025-03-09 16:30:49 +0800

    header refactor

--- arcana-ecommerce/src/components/Header.tsx
+++ arcana-ecommerce/src/components/Header.tsx
@@ -2,16 +2,22 @@ import { FaSearch, FaUser, FaShoppingCart } from "react-icons/fa";
 
 export function Header() {
     return (
-        <div className="d-flex bg-white w-full h-16 font-ibmplexmono flex flex-row items-center px-4 z-10 justify-between">
-            <div className="flex flex-row gap-10">
-                <a className="text-black text-base font-bold">PRODUCTS</a>
-                <a className="text-black text-base font-bold">ABOUT US</a>
+        <div className="d-flex bg-white w-full h-20 font-ibmplexmono flex flex-row items-center px-8 z-10 justify-between">
+            <div className="flex flex-row w-48 justify-between">
+                <button className="text-black text-base font-bold">PRODUCTS</button>
+                <button className="text-black text-base font-bold">ABOUT US</button>
             </div>
-            <a className="text-black text-base font-bold">PA</a>
-            <div className="flex flex-row gap-4">
-                <FaSearch color="black" />
-                <FaUser color="black" />
-                <FaShoppingCart color="black" />
+            <a className="text-black font-italianoldstyle font-black text-3xl">PA</a>
+            <div className="flex flex-row gap-8 w-48 justify-end">
+                <button className="">
+                    <FaSearch color="black" />
+                </button>
+                <button className="">
+                    <FaUser color="black" />
+                </button>
+                <button className="">
+                    <FaShoppingCart color="black" />
+                </button>
             </div>
         </div>
     )
