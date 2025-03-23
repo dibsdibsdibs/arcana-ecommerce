982249ec8edb0831132be384bb3610c3f4972e3e
Divine <divinelavente@gmail.com>
2025-03-23 18:20:25 +0800

    about us

--- arcana-ecommerce/src/app/page.tsx
+++ arcana-ecommerce/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "@/components/HeroSection";
 import { ProductListCategory } from "@/components/ProductListCategory";
 import { FeaturedProducts } from "@/components/FeaturedProducts";
+import { AboutUs } from "@/components/AboutUs";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <ProductListCategory />
       <FeaturedProducts />
+      <AboutUs />
     </div>
   );
 }
