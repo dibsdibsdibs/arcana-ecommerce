af1fdde3d427082440fe51ffc801ac7ff5bb312c
Divine <divinelavente@gmail.com>
2025-03-23 21:10:42 +0800

    set up dif categories

--- arcana-ecommerce/src/app/page.tsx
+++ arcana-ecommerce/src/app/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+import { useState, useEffect } from "react";
 import { HeroSection } from "@/components/HeroSection";
 import { ProductListCategory } from "@/components/ProductListCategory";
 import { FeaturedProducts } from "@/components/FeaturedProducts";
 import { AboutUs } from "@/components/AboutUs";
+import sampledata from "@/public/sampledata.json";
+import { Product } from "@/model/products";
 
 export default function Home() {
+  const category = sampledata.products.map((category: any) => category.category);
+  const featuredProducts = sampledata.featuredProducts;
+  const [displayedCategory, setDisplayedCategory] = useState(category[0]);
+  const [displayedProducts, setDisplayedProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const list = sampledata.products.find((category: any) => category.category === displayedCategory);
+    setDisplayedProducts(list?.products as Product[]);
+  }, [displayedCategory]);
+  
   return (
     <div className="bg-cream">
       <HeroSection />
       <ProductListCategory />
-      <FeaturedProducts />
+      <FeaturedProducts featuredProducts={displayedProducts} />
       <AboutUs />
     </div>
   );
---
+++ arcana-ecommerce/src/model/products.tsx
@@ -0,0 +1,8 @@
+export interface Product {
+    id: number;
+    name: string;
+    image: string;
+    price: string;
+    ratings: number;
+    reviews: number;
+}
